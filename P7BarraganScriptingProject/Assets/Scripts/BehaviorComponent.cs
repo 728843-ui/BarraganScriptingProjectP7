2dbd9237812d0b794babc59d4e8706b785049c3f
728843-ui <728843@student.dusd.net>
2025-10-28 14:03:06 -0700

    yup
    yup

--- P7BarraganScriptingProject/Assets/Scripts/BehaviorComponent.cs
+++ P7BarraganScriptingProject/Assets/Scripts/BehaviorComponent.cs
@@ -7,20 +7,25 @@ public class BehaviorComponent : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        if put.GetKeyDown(KeyCode.R))
-        { 
-       Getomponent<Renderer>().material = Color.red;
-    }
-    if (Input.GetKeyDown(KeyCode.G)
-    }
-        GetComponent<Renderer>().material.color = Color.green;
-    }
-    if (Input.GetKeyDown(KeyCode.B))
-    }
-        GetComponent<Renderer>().material.color = Color.blue;
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            GetComponent<Renderer>().material.color = Color.red;
+        }
+
+        if (Input.GetKeyDown(KeyCode.G))
+        {
+            GetComponent<Renderer>().material.color = Color.green;
+        }
+
+        if (Input.GetKeyDown(KeyCode.B))
+        {
+            GetComponent<Renderer>().material.color = Color.blue;
+        }       
+     } 
+  }
